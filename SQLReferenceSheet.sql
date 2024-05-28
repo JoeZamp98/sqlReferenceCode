b4388f0b8212f0841f0dbd70d43f37179fe2ffdf
joezamp98 <joezamp98@gmail.com>
2024-05-27 23:28:15 -0400

    Added database creation/deletion examples.

---
+++ SQLReferenceSheet.sql
@@ -0,0 +1,9 @@
+-- Active: 1670470955994@@localhost@3306@riskified
+-- CREATE / DROP DATABASE --
+CREATE DATABASE weather_warnings;
+
+SHOW DATABASES;
+
+#DROP DATABASE weather_warnings;
+
+-- CREATE TABLES --
